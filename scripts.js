d7022ddc0a91cc06fe2913338a277485c03d338f
Verity Griscti <44616764+veritygriscti@users.noreply.github.com>
2021-08-15 12:18:03 -0400

    sticky navbar, frethero case study page

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// Sticky Navbar
+
+window.onscroll = function() {stickyNav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 

5f79fe4335da88efcabd43125d0c133c526f6ac6
Verity Griscti <44616764+veritygriscti@users.noreply.github.com>
2022-04-28 16:51:11 -0400

    fixed broken links, updated reso pdf

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 // Sticky Navbar
 
-window.onscroll = function() {stickyNav()};
+window.onscroll = function () {
+  stickyNav();
+};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
@@ -8,12 +10,8 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
-    navbar.classList.remove("sticky")
+    navbar.classList.remove("sticky");
   }
-} 
-
-
-
-
+}
